83b26a1353456d1e31ac51e86ffe15c47aaf5c4a
Wildan Bimantoro <wildan.bimantoro@aitindo.com>
2017-05-29 22:42:28 +0700

    Init commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.port || 3000;
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port,() => {
+	console.log(`Server is up on ${port}`);
+	console.log(publicPath);
+});
