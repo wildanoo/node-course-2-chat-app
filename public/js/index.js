ce798a4b23e9ce82763a198282b48f51b946fa4e
Wildan Bimantoro <wildan.bimantoro@aitindo.com>
2017-06-01 16:44:49 +0700

    Greet new user, and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -12,3 +12,10 @@ socket.on('newMessage',function(message){
 	console.log('newMessage', message);
 });
 
+socket.on('joinApp',function(message){
+	console.log('Welcome message: ', message.text)
+});
+
+socket.on('newUser',function(message){
+	console.log('Notification: ',message.text);
+});
